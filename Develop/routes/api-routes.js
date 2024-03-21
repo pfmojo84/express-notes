de3b79c5d29929724fd8aaf52ce305d7047daed5
jdporr <jdporrazzo@gmail.com>
2024-03-21 14:25:12 -0400

    updated api routes with async await instructions

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -4,25 +4,24 @@ const path = require ('path');
 const { v4: uuidv4 } = require('uuid');
 
 //create get request to read db.json file 
-router.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
+router.get('/api/notes', async (req, res) => {
+    const dbJSON = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    res.json(dbJSON);
 });
 
 //create post route to receive new note and save on the request body and add it to the db.json file
 router.post('/api/notes', (req, res) => {
-    let db = fs.readFileSync('db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
+    const dbJSON = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
     //create body for new note
-    let newNote = {
+    const newNote = {
         title: req.body.title,
         text: req.body.text,
         id: uuidv4(),
     };
     //push new note to be written on the db.json file
-    db.push(newNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(db);
+    dbJSON.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbJSON));
+    res.json(dbJSON);
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -3,16 +3,15 @@ const express = require ('express');
 const htmlRoutes = require('./routes/html-routes');
 const apiRoutes = require('./routes/api-routes');
 
-//triggers express built in method(s) -- double check accuracy
-const app = express();
-
 //create environment variable port
 const PORT = process.env.port || 3001;
 
+//triggers express built in method(s) -- double check accuracy
+const app = express();
 
 //set middleware for parsing json and url-encoded data
+app.use(express.urlencoded({ extended: false})); //clarification needed
 app.use(express.json());
-app.use(express.urlencoded({ extended: true})); //clarification needed
 app.use(express.static('public'));
 app.use(htmlRoutes);
 app.use(apiRoutes);
